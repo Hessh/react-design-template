4f3d3b017b44793d722273a085134193b3dfcb60
Haakon Hess <haakon.hess@gmail.com>
2020-05-17 03:09:14 +0200

    Click event styling

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -16,7 +16,17 @@ export class Header extends Component {
     this.headerRight = React.createRef();
   }
 
-  toggleMenu = () => {
+  toggleMenu = (e) => {
+    /** Desktop styling */
+    let linkArray = this.headerRight.current.childNodes;
+    for (let i = 0; i < linkArray.length; i++) {
+      linkArray[i].style.color = "#888";
+      linkArray[i].style.textDecoration = "none";
+    }
+    e.target.style.color = "#333";
+    e.target.style.textDecoration = "underline";
+
+    /** Burger menu */
     if (!showMenu && window.innerWidth < 1000) {
       this.headerRight.current.style.display = "flex";
       this.header.current.style.boxShadow = "none";
@@ -30,44 +40,52 @@ export class Header extends Component {
     }
   };
 
+  toggleHome = () => {
+    let linkArray = this.headerRight.current.childNodes;
+    for (let i = 0; i < linkArray.length; i++) {
+      linkArray[i].style.color = "#888";
+      linkArray[i].style.textDecoration = "none";
+    }
+  };
+
   render() {
     return (
       <header ref={this.header}>
         <div className="header-left">
-          <Link to="/" className="link">
+          <Link to="/" className="link" onClick={this.toggleHome}>
             <span>Design Template</span>
           </Link>
         </div>
         <div className="header-right" ref={this.headerRight}>
-          <Link to="/layout" className="link" onClick={this.toggleMenu}>
-            <span>LAYOUT</span>
+          <Link className="link" onClick={this.toggleMenu} to="/layout">
+            LAYOUT
           </Link>
-          <Link to="/colors" className="link" onClick={this.toggleMenu}>
-            <span>COLORS</span>
+          <Link className="link" onClick={this.toggleMenu} to="/colors">
+            COLORS
           </Link>
-          <Link to="/fonts" className="link" onClick={this.toggleMenu}>
-            <span>FONTS</span>
+          <Link className="link" onClick={this.toggleMenu} to="/fonts">
+            FONTS
           </Link>
-          <Link to="/icons" className="link" onClick={this.toggleMenu}>
-            <span>ICONS</span>
+          <Link className="link" onClick={this.toggleMenu} to="/icons">
+            ICONS
           </Link>
-          <Link to="/illustrations" className="link" onClick={this.toggleMenu}>
-            <span>ILLUSTRATIONS</span>
+          <Link className="link" onClick={this.toggleMenu} to="/illustrations">
+            ILLUSTRATIONS
           </Link>
-          <Link to="/buttons" className="link" onClick={this.toggleMenu}>
-            <span>BUTTONS</span>
+          <Link className="link" onClick={this.toggleMenu} to="/buttons">
+            BUTTONS
           </Link>
-          <Link to="/links" className="link" onClick={this.toggleMenu}>
-            <span>LINKS</span>
+          <Link className="link" onClick={this.toggleMenu} to="/links">
+            LINKS
           </Link>
-          <Link to="/transitions" className="link" onClick={this.toggleMenu}>
-            <span>TRANSITIONS</span>
+          <Link className="link" onClick={this.toggleMenu} to="/transitions">
+            TRANSITIONS
           </Link>
-          <Link to="/animations" className="link" onClick={this.toggleMenu}>
-            <span>ANIMATIONS</span>
+          <Link className="link" onClick={this.toggleMenu} to="/animations">
+            ANIMATIONS
           </Link>
-          <Link to="/content" className="link" onClick={this.toggleMenu}>
-            <span>CONTENT</span>
+          <Link className="link" onClick={this.toggleMenu} to="/content">
+            CONTENT
           </Link>
         </div>
         <FontAwesomeIcon
