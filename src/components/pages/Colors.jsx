428fdbcd42425fbfe8c4c5adcb308ba6aa11af19
maricathrinekarlsen <nerdmary1@gmail.com>
2020-05-19 12:24:34 +0200

    farger m komplimentaer

--- src/components/pages/Colors.jsx
+++ src/components/pages/Colors.jsx
@@ -8,7 +8,7 @@ export class Colors extends Component {
       <div className="container">
         {/* Palett nr 1 */}
         <div className="palette-row palette-row-1">
-          <h4>Primary: Miljøfarger 1</h4>
+          <h4>Primary: Environment</h4>
           <div className="palette-container">
             <div className="palette-wrapper">
               <div className="color-box box-1"></div>
@@ -30,7 +30,29 @@ export class Colors extends Component {
         </div>
         {/* Palett nr 2 */}
         <div className="palette-row palette-row-2">
-          <h4>Primary: Blå classy</h4>
+          <h4>Complimentary: Environment</h4>
+          <div className="palette-container">
+            <div className="palette-wrapper">
+              <div className="color-box box-1"></div>
+              <p className="color-label">#59463C</p>
+            </div>
+            <div className="palette-wrapper">
+              <div className="color-box box-2"></div>
+              <p className="color-label">#3FC7B2</p>
+            </div>
+            <div className="palette-wrapper">
+              <div className="color-box box-3"></div>
+              <p className="color-label">#A0FAEC</p>
+            </div>
+            <div className="palette-wrapper">
+              <div className="color-box box-4"></div>
+              <p className="color-label">#C0D6D1</p>
+            </div>
+          </div>
+        </div>
+        {/* Palett nr 3 */}
+        <div className="palette-row palette-row-3">
+          <h4>Primary: Blue business</h4>
           <div className="palette-container">
             <div className="palette-wrapper">
               <div className="color-box box-1"></div>
@@ -50,9 +72,31 @@ export class Colors extends Component {
             </div>
           </div>
         </div>
-        {/* Palett nr 3 */}
-        <div className="palette-row palette-row-3">
-          <h4>Primary: Miljøfarger 2</h4>
+        {/* Palett nr 4 */}
+        <div className="palette-row palette-row-4">
+          <h4>Complimentary: Blue business</h4>
+          <div className="palette-container">
+            <div className="palette-wrapper">
+              <div className="color-box box-1"></div>
+              <p className="color-label">#2B1C00</p>
+            </div>
+            <div className="palette-wrapper">
+              <div className="color-box box-2"></div>
+              <p className="color-label">#FFA03D</p>
+            </div>
+            <div className="palette-wrapper">
+              <div className="color-box box-3"></div>
+              <p className="color-label">#FFBA68</p>
+            </div>
+            <div className="palette-wrapper">
+              <div className="color-box box-4"></div>
+              <p className="color-label">#FFE1B9</p>
+            </div>
+          </div>
+        </div>
+        {/* Palett nr 5 */}
+        <div className="palette-row palette-row-5">
+          <h4>Primary: Environment cold</h4>
           <div className="palette-container">
             <div className="palette-wrapper">
               <div className="color-box box-1"></div>
@@ -72,25 +116,69 @@ export class Colors extends Component {
             </div>
           </div>
         </div>
-         {/* Palett nr 4 */}
-         <div className="palette-row palette-row-4">
-          <h4>Primary: Miljøfarger 3</h4>
+        {/* Palett nr 6 */}
+        <div className="palette-row palette-row-6">
+          <h4>Complimentary: Environment cold</h4>
           <div className="palette-container">
             <div className="palette-wrapper">
               <div className="color-box box-1"></div>
-              <p className="color-label">#1F5221</p>
+              <p className="color-label">#66180C</p>
             </div>
             <div className="palette-wrapper">
               <div className="color-box box-2"></div>
-              <p className="color-label">#328535</p>
+              <p className="color-label">#B35134</p>
             </div>
             <div className="palette-wrapper">
               <div className="color-box box-3"></div>
-              <p className="color-label">#599E5B</p>
+              <p className="color-label">#B3604B</p>
             </div>
             <div className="palette-wrapper">
               <div className="color-box box-4"></div>
-              <p className="color-label">#9BD19D</p>
+              <p className="color-label">#FCDCD6</p>
+            </div>
+          </div>
+        </div>
+         {/* Palett nr 7 */}
+         <div className="palette-row palette-row-7">
+          <h4>Primary: Environment strong</h4>
+          <div className="palette-container">
+            <div className="palette-wrapper">
+              <div className="color-box box-1"></div>
+              <p className="color-label">#2B1C00</p>
+            </div>
+            <div className="palette-wrapper">
+              <div className="color-box box-2"></div>
+              <p className="color-label">#FFA03D</p>
+            </div>
+            <div className="palette-wrapper">
+              <div className="color-box box-3"></div>
+              <p className="color-label">#FFBA68</p>
+            </div>
+            <div className="palette-wrapper">
+              <div className="color-box box-4"></div>
+              <p className="color-label">#FFC87A</p>
+            </div>
+          </div>
+        </div>
+        {/* Palett nr 8 */}
+        <div className="palette-row palette-row-8">
+          <h4>Primary: Innovation, new thinking</h4>
+          <div className="palette-container">
+            <div className="palette-wrapper">
+              <div className="color-box box-1"></div>
+              <p className="color-label">#DB852C</p>
+            </div>
+            <div className="palette-wrapper">
+              <div className="color-box box-2"></div>
+              <p className="color-label">#FFBC67</p>
+            </div>
+            <div className="palette-wrapper">
+              <div className="color-box box-3"></div>
+              <p className="color-label">#FFE478</p>
+            </div>
+            <div className="palette-wrapper">
+              <div className="color-box box-4"></div>
+              <p className="color-label">#FFF1BB</p>
             </div>
           </div>
         </div>
