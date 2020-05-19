c7c8a5d5be89abed0bda06fff55c88f5bedbf7a1
Haakon Hess <haakon.hess@gmail.com>
2020-05-19 17:15:22 +0200

    Made some button-examples

--- src/components/pages/Buttons.jsx
+++ src/components/pages/Buttons.jsx
@@ -5,41 +5,107 @@ import "../../css/pages/buttons.css";
 export class Buttons extends Component {
   render() {
     return (
-      <div className="container">
+      <div className="container btn-main-container">
         <div className="btn-container">
-          <h4>Default button</h4>
+          <h4>Square flat buttons</h4>
+          <h5>CTA</h5>
           <div className="btn-wrapper">
-            <button className="default-button">Default</button>
+            <button className="square-flat cta-button green">
+              Placeholder
+            </button>
+            <button className="square-flat cta-button strong-green">
+              Placeholder
+            </button>
+          </div>
+          <h5>Filter</h5>
+          <div className="btn-wrapper">
+            <button className="square-flat filter-button">Placeholder</button>
+            <button className="square-flat filter-button">Placeholder</button>
           </div>
         </div>
         <div className="btn-container">
-          <h4>CTA buttons</h4>
+          <h4>Square shadow buttons</h4>
+          <h5>CTA</h5>
           <div className="btn-wrapper">
-            <button className="cta-button">Kom igang nå</button>
+            <button className="square-shadow cta-button green">
+              Placeholder
+            </button>
+            <button className="square-shadow cta-button strong-green">
+              Placeholder
+            </button>
+          </div>
+          <h5>Filter</h5>
+          <div className="btn-wrapper">
+            <button className="square-shadow filter-button">Placeholder</button>
+            <button className="square-shadow filter-button">Placeholder</button>
           </div>
         </div>
         <div className="btn-container">
-          <h4>Continue buttons</h4>
+          <h4>Rounded flat buttons</h4>
+          <h5>CTA</h5>
           <div className="btn-wrapper">
-            <button className="continue-button">Fortsett</button>
+            <button className="rounded-flat cta-button green">
+              Placeholder
+            </button>
+            <button className="rounded-flat cta-button strong-green">
+              Placeholder
+            </button>
           </div>
+          <h5>Filter</h5>
           <div className="btn-wrapper">
-            <button className="continue-button">Videre</button>
+            <button className="rounded-flat filter-button">Placeholder</button>
+            <button className="rounded-flat filter-button">Placeholder</button>
           </div>
         </div>
         <div className="btn-container">
-          <h4>Finish buttons</h4>
+          <h4>Rounded shadow buttons</h4>
+          <h5>CTA</h5>
           <div className="btn-wrapper">
-            <button className="finish-button">Fullfør</button>
+            <button className="rounded-shadow cta-button green">
+              Placeholder
+            </button>
+            <button className="rounded-shadow cta-button strong-green">
+              Placeholder
+            </button>
+          </div>
+          <h5>Filter</h5>
+          <div className="btn-wrapper">
+            <button className="rounded-shadow filter-button">
+              Placeholder
+            </button>
+            <button className="rounded-shadow filter-button">
+              Placeholder
+            </button>
           </div>
         </div>
-        <div className="btn-container">
-          <h4>Log in/out -buttons</h4>
+        <div className="btn-container btn-hover-container">
+          <h4>Hover effects</h4>
+          <h5>Size</h5>
           <div className="btn-wrapper">
-            <button className="login-button">Fullfør</button>
+            <button className="square-flat cta-button green hover-size">
+              Placeholder
+            </button>
+            <button className="square-flat cta-button strong-green hover-size">
+              Placeholder
+            </button>
           </div>
+          <h5>Shadow</h5>
           <div className="btn-wrapper">
-            <button className="logout-button">Fullfør</button>
+            <button className="square-shadow cta-button green hover-shadow">
+              Placeholder
+            </button>
+            <button className="square-shadow cta-button strong-green hover-shadow">
+              Placeholder
+            </button>
+          </div>
+          <h5>Color</h5>
+          <div className="btn-wrapper">
+            <button className="square-flat cta-button green hover-color-green">
+              Placeholder
+            </button>
+            <button className="square-flat cta-button strong-green hover-color-strong-green">
+              Placeholder
+            </button>
           </div>
         </div>
       </div>
