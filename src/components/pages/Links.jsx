e73c02c0210c1bffd0527dbced69925646d2d51f
Haakon Hess <haakon.hess@gmail.com>
2020-05-19 17:53:40 +0200

    Made some link examples

--- src/components/pages/Links.jsx
+++ src/components/pages/Links.jsx
@@ -1,8 +1,32 @@
 import React, { Component } from "react";
 
+import "../../css/pages/links.css";
+
 export class Links extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="container">
+        <div className="link-container">
+          <h4>Outbound links</h4>
+          <h5>Example 1:</h5>
+          <div className="link-wrapper example-1">
+            <p>
+              Lorem ipsum dolor sit,{" "}
+              <a href="https://github.com/Leifhaa">amet consectetur</a>{" "}
+              adipisicing elit. Aspernatur, ipsum.
+            </p>
+          </div>
+          <h5>Example 2:</h5>
+          <div className="link-wrapper example-2">
+            <p>
+              Lorem ipsum dolor sit,{" "}
+              <a href="https://github.com/Hulliskoa">amet consectetur</a>{" "}
+              adipisicing elit. Aspernatur, ipsum.
+            </p>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
